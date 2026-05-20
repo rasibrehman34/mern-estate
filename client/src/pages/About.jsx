c403f35a354a98bbae47e049f1d88a4cd4f6e76f
Rasib Rehman <rasibrehman34@gmail.com>
2026-05-21 00:39:33 +0500

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      aboutsn
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl text-slate-800 font-bold mb-4'>
+        About Sahand Estate
+      </h1>
+      <p className='mb-4 text-slate-700'>
+        Sahand Estate is a leading real estate company dedicated to providing exceptional service and quality properties to our clients. With years of experience in the industry, we strive to meet all your real estate needs with professionalism and integrity.
+      </p>  
+      <p className='mb-4 text-slate-700'>Sahand Estate is a leading real estate company dedicated to providing exceptional service and quality properties to our clients. With years of experience in the industry, we strive to meet all your real estate needs with professionalism and integrity.</p>
+
+      <p className='mb-4 text-slate-700'>Sahand Estate is a leading real estate company dedicated to providing exceptional service and quality properties to our clients. With years of experience in the industry, we strive to meet all your real estate needs with professionalism and integrity.</p>
     </div>
   )
 }
